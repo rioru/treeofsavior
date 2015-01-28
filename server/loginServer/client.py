f98d2be24ee3488e625292d9fb108619aa0d4047
Rioru <rioru@seraphicsquad.com>
2015-01-28 05:38:54 +0100

    Update BC_COMMANDER_CREATE to allow hairstyle changes

--- server/loginServer/client.py
+++ server/loginServer/client.py
@@ -9,7 +9,7 @@ class ClientHandler:
 	# Configurations
 	PACKETSIZE_MAX = 8192
 
-	def loginOKHandler (self, sock): 
+	def loginOKHandler (self, sock):
 		print "BC_LOGINOK Handler";
 		data = sock.recv (self.PACKETSIZE_MAX)
 		print 'CB_LOGIN_BY_PASSPORT expected. Received : ' + binascii.hexlify(data)
@@ -18,7 +18,7 @@ class ClientHandler:
 		reply += "\x00" * 35 # <-- Zéro ici pour mettre le compte en mode développeur
 		sock.send (reply)
 		print "Sent : " + binascii.hexlify (reply);
-		
+
 	def startBarrack (self, sock):
 		data = sock.recv (self.PACKETSIZE_MAX)
 		print 'CB_START_BARRACK expected. Received : ' + binascii.hexlify(data)
@@ -58,7 +58,7 @@ class ClientHandler:
 			print "BC_COMMANDER_CREATE Handler";
 			data = sock.recv (self.PACKETSIZE_MAX) # CB_BARRACKNAME_CHANGE
 			print 'Expected CB_COMMANDER_CREATE. Received : ' + binascii.hexlify(data)
-			
+
 			# Size: 318
 			# Configs
 			nbCharacterBarrack += 1;
@@ -69,44 +69,45 @@ class ClientHandler:
 			characterLevel = 1337;
 			# 0x2710 = empty item (? or maybe it's just bugged)
 			itemsID = [0x9d89d, 0x9d89e, 0x9d89f, 0x9d8a0, 0x9d8a1, 0x2710, 0x2710, 0x2710, 0x9d8a5, 0x9d8a6, 0x9d8a7, 0x9d8a8, 0x9d8a9, 0x9d8aa, 0x9d8ab, 0x9d8ac, 0x9d8ad, 0x9d8ae, 0x9d8af, 0x9d8b0];
-			spriteID = 2;
-			
+			hairId = 2;
+			spriteID = 2; # max 18
+
 			# Packet
 			reply  = struct.pack("<H", PacketType.BC_COMMANDER_CREATE);
 			reply += struct.pack("<I", 0xFF) * 37; # UNKNOWN
-			
+
 			reply += struct.pack("<H", classId) # class id
-			
+
 			reply += struct.pack("<H", 0xFF) # UNKNOWN
-			
+
 			reply += struct.pack("<H", jobId) # job id
 			reply += struct.pack("<B", gender) # Gender
-			
+
 			reply += struct.pack("<B", 1) # UNKNOWN
-			
+
 			reply += struct.pack("<I", characterLevel); # Character level
-			
+
 			for itemID in itemsID: # Inventory : 20 items
 				reply += struct.pack("<I", itemID); # items
-			
-			reply += struct.pack("<Q", 0xFF); # UNKNOWN
+
+			reply += struct.pack("<Q", hairId); # Hairstyle
 			reply += struct.pack("<I", 0xFF) # UNKNOWN
-			
+
 			reply += struct.pack("<B", nbCharacterBarrack) # Character position in the character list
-			
+
 			reply += struct.pack("<B", 0xFF) # UNKNOWN
-			
+
 			reply += struct.pack("<H", mapId) # mapID
-			
+
 			reply += struct.pack("<I", 0xFF) * 3; # UNKNOWN
 			reply += struct.pack("<I", spriteID) * 1; # Apparence du sprite du corps
 			reply += struct.pack("<I", 0xFF) * 1; # UNKNOWN
 			reply += struct.pack("<I", 0xFF) * 2; # UNKNOWN
 			reply += struct.pack("<I", 0xFF) * 8; # UNKNOWN
-			
+
 			sock.send (reply)
 			print "Sent : " + binascii.hexlify (reply);
-			
+
 
 	"""
 	def iesModifyListHandler (self, sock):
@@ -118,7 +119,7 @@ class ClientHandler:
 		reply += "B" * 82
 		sock.send (reply);
 		print "Sent : " + binascii.hexlify (reply);
-		
+
 	def AccountPropHandler (self, sock):
 		time.sleep(3);
 		print "BC_ACCOUNT_PROP Handler";
@@ -128,7 +129,7 @@ class ClientHandler:
 		reply += "B" * 82
 		sock.send (reply);
 		print "Sent : " + binascii.hexlify (reply);
-		
+
 	def singleInfoHandler (self, sock):
 		time.sleep(3);
 		print "BC_SINGLE_INFO";
@@ -137,7 +138,7 @@ class ClientHandler:
 		reply += "A" * 335
 		sock.send (reply)
 		print "Sent : " + binascii.hexlify (reply);
-		
+
 	def echoHandler (self, sock):
 		time.sleep(3);
 		print "BC_ECHO";
@@ -155,7 +156,7 @@ class ClientHandler:
 		self.currentBarrackHandler (sock);
 		self.barrackNameChangeHandler (sock);
 		self.commanderCreateHandler (sock);
-		
+
 	def __init__ (self, sock):
 		self.sock = sock;
 		self.start (sock);
