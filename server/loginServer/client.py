e6a7549efb6e25be93e4d40481e0d090a6d00ee3
Spl3en <spl3en.contact@gmail.com>
2015-02-03 21:00:21 +0100

    Hude refactoring of the login server : Read *decrypted packets* and handle packets based on their packetType.

--- server/loginServer/client.py
+++ server/loginServer/client.py
@@ -7,37 +7,43 @@ from PacketType import PacketType
 
 # ==========================================================
 class ClientHandler:
+
 	# Configurations
 	PACKETSIZE_MAX = 8192
 
-	def loginOKHandler (self, sock):
-		print "BC_LOGINOK Handler";
-		data = sock.recv (self.PACKETSIZE_MAX)
-		print 'CB_LOGIN_BY_PASSPORT expected. Received : ' + binascii.hexlify(data)
-		# Size: 37
+	nbCharacterBarrack = 0;
+	positionCharacterList = 0;
+
+	def loginByPasswordHandler (self, packet):
+		# CB_LOGIN_BY_PASSPORT = 0x0004, // Size: 531
+		print 'CB_LOGIN_BY_PASSPORT expected. Received : ' + binascii.hexlify (packet) + " (" + str(len(packet)) + ")";
+
 		reply  = struct.pack("<H", PacketType.BC_LOGINOK)
 		reply += "\x00" * 8
 		reply += "\x01" * 1 # Send movements to server in barrack + allow mouse movements
 		reply += "\x00" * 24
 		reply += "\x00" * 1 # 0 = developper mode, otherwise normal mode
 		reply += "\x00" * 1
-		sock.send (reply)
+
+		self.sock.send (reply)
 		print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
 
-	def startBarrackHandler (self, sock):
-		data = sock.recv (self.PACKETSIZE_MAX)
-		print 'CB_START_BARRACK expected. Received : ' + binascii.hexlify(data)
 
-	def serverEntryHandler (self, sock):
+	def startBarrackHandler (self, packet):
+		# CB_START_BARRACK = 0x0006, // Size: 11
+		print 'CB_START_BARRACK expected. Received : ' + binascii.hexlify (packet) + " (" + str(len(packet)) + ")";
+		self.commanderListHandler (packet);
+
+
+	def serverEntryHandler (self, packet):
+		# BC_SERVER_ENTRY = 0x0057, // Size: 18
 		channelIP = "127.0.0.1";
 		channelPort1 = 1337;
 		channelPort2 = 1338;
-	
-		# Size = 18
-		print "BC_SERVER_ENTRY Handler";
+
 		reply  = struct.pack("<H", PacketType.BC_SERVER_ENTRY)
 		reply += struct.pack("<I", 0); # UNKNOWN
-		
+
 		channelIP = socket.inet_aton (channelIP);
 		reply += channelIP; # ClientNet
 		reply += channelIP; # VirtualClientNet
@@ -45,15 +51,15 @@ class ClientHandler:
 		reply += struct.pack("<H", channelPort1);
 		reply += struct.pack("<H", channelPort2);
 
-		sock.send (reply);
+		self.sock.send (reply);
 		print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
 
-	def commanderListHandler (self, sock):
-		# configs
+		
+	def commanderListHandler (self, packet):
+		# BC_COMMANDER_LIST = 0x000F, // Size: 0
 		createNewAccount = True;
 		nbCharacterListed = 1;
 	
-		print "BC_COMMANDER_LIST Handler";
 		reply  = struct.pack("<H", PacketType.BC_COMMANDER_LIST)
 		reply += struct.pack("<I", 0); # UNKNOWN
 
@@ -67,26 +73,27 @@ class ClientHandler:
 			reply += struct.pack("<B", nbCharacterListed); # Number of characters contained in the packet
 			reply += barrackName + "\x00" * (64 - len(barrackName));
 
-			data = struct.pack("<H", 0x1759); # ClassListID
+			data = struct.pack("<H", 0x1e8); # Barrack class ID
 			reply += struct.pack("<H", len(data)); # Size of data following this field
 			reply += data;
 
 			# CommanderCreatePacket-like
-			nbCharacterBarrack = 1;
-			positionCharacterList = 1;
+			self.nbCharacterBarrack += 1;
+			self.positionCharacterList += 1;
 			charName = "Rioru";
 			rightClick = "I'm displayed by a right click on the character";
-			classId = 10004;
-			jobId = 4;
-			gender = 1;
-			mapId = 0xD34;
+			classId = 10005; # 10001 = Warrior, 10006 = Mage, 10003 = Archer, 10005 = Cleric
+			jobId = 4;       # 1     = Warrior, 2     = Mage, 3     = Archer, 4     = Cleric
+			gender = 2;
+			mapId = 0x408;
 			characterLevel = 1337;
-			itemsId = [0x9d89d, 0x9d89e, 0x9d89f, 0x9d8a0, 
-			           0x9d8a1, 0x02710, 0x02710, 0x02710, 
-					   0x9d8a5, 0x9d8a6, 0x9d8a7, 0x9d8a8, 
-					   0x9d8a9, 0x9d8aa, 0x9d8ab, 0x9d8ac, 
-					   0x9d8ad, 0x9d8ae, 0x9d8af, 0x9d8b0];
-			hairId = 2;
+			# 0x2710 = no helmet (empty slot)
+			itemsId = [0x00002710, 0x00099536, 0x00002710, 0x00081E91,  # Head 1   | Head 2     |    ?                               |  Armor
+			           0x0007A959, 0x0007D071, 0x00002710, 0x000933E0,  # Gloves   | Boots      |    ? (makes the head disappear xD) |  Bracelet
+					   0x00027D20, 0x00036511, 0x00098208, 0x00002710,  # Bow      | Shield     |   Costume                          |   ?
+					   0x0007F782, 0x00099562, 0x0007F3A1, 0x00081AAC, 	# ?        |  ?         |   Pants                            |   ?
+					   0x00002710, 0x00092C1A, 0x00092C1B, 0x0008DE0B];	# ?        | Ring left  |   Right right                      |  Necklace
+			hairId = 3;
 			spriteID = 0; # max 18
 
 			# Packet
@@ -101,13 +108,17 @@ class ClientHandler:
 			reply += struct.pack("<B", gender) # Gender
 			reply += struct.pack("<B", 1) # UNKNOWN
 			reply += struct.pack("<I", characterLevel); # Character level
+
 			for itemId in itemsId: # Inventory : 20 items
 				reply += struct.pack("<I", itemId); # items
+
 			reply += struct.pack("<B", hairId); # Hairstyle
-			reply += struct.pack("<B", 0) * 3; # UNKNOWN
-			reply += struct.pack("<I", 0); # UNKNOWN
-			reply += struct.pack("<I", positionCharacterList) # Position in the character list
-			reply += struct.pack("<B", nbCharacterBarrack) # Character position in the character list
+			reply += struct.pack("<B",0x00) * 3; # UNKNOWN
+			
+			reply += struct.pack("<I",0x000000FF); # PCID - Still need to understand how it works
+
+			reply += struct.pack("<I", self.positionCharacterList) # Position in the character list
+			reply += struct.pack("<B", self.nbCharacterBarrack) # Character position in the character list
 			reply += struct.pack("<B", 0) # UNKNOWN
 			reply += struct.pack("<H", mapId) # mapID
 			reply += struct.pack("<I", 0) * 3; # UNKNOWN
@@ -120,90 +131,123 @@ class ClientHandler:
 		size = struct.pack("<H", len(reply) + 2); # +2 because it counts itself
 		reply = reply[:6] + size + reply[6:];
 
-		sock.send (reply);
+		self.sock.send (reply);
 		print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
 
-	def currentBarrackHandler (self, sock):
-		print "CURRENT_BARRACK Handler";
-		data = sock.recv (self.PACKETSIZE_MAX) # CURRENT_BARRACK
-		print 'Expected CURRENT_BARRACK. Received : ' + binascii.hexlify(data)
+	def currentBarrackHandler (self, packet):
+		# CB_CURRENT_BARRACK = 0x004E, // Size: 39
+		print 'CB_CURRENT_BARRACK expected. Received : ' + binascii.hexlify (packet) + " (" + str(len(packet)) + ")";
 
-	def barrackNameChangeHandler (self, sock):
-		print "BC_BARRACKNAME_CHANGE Handler";
-		data = sock.recv (self.PACKETSIZE_MAX) # CB_BARRACKNAME_CHANGE
-		print 'Expected CB_BARRACKNAME_CHANGE. Received : ' + binascii.hexlify(data)
+	def barrackNameChangeHandler (self, packet):
+		# CB_BARRACKNAME_CHANGE = 0x000A, // Size: 74
+		print 'CB_BARRACKNAME_CHANGE expected. Received : ' + binascii.hexlify (packet) + " (" + str(len(packet)) + ")";
+		
+		# BC_BARRACKNAME_CHANGE = 0x0017, // Size: 75
 		barrackName = "Spl3en"
 		reply  = struct.pack("<H", PacketType.BC_BARRACKNAME_CHANGE)
 		reply += "\x01" * 9;
 		reply += barrackName + "\x00" * (64 - len(barrackName));
-		sock.send (reply)
+		self.sock.send (reply)
 		print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
 
-	def commanderCreateHandler (self, sock):
-		nbCharacterBarrack = 0;
-		positionCharacterList = 0;
+	def commanderCreateHandler (self, packet):
+		# CB_COMMANDER_CREATE = 0x0007, // Size: 91
+		print 'Expected CB_COMMANDER_CREATE. Received : ' + binascii.hexlify(packet) + " (" + str(len(packet)) + ")";
 
+		# BC_COMMANDER_CREATE = 0x0010, // Size: 318
+		self.nbCharacterBarrack += 1;
+		self.positionCharacterList += 1;
+		charName = "Rioru";
+		rightClick = "I'm displayed by a right click on the character";
+		classId = 10005; # 10001 = Warrior, 10006 = Mage, 10003 = Archer, 10005 = Cleric
+		jobId = 4;       # 1     = Warrior, 2     = Mage, 3     = Archer, 4     = Cleric
+		gender = 2;
+		mapId = 0x408;
+		characterLevel = 1337;
+		# 0x2710 = no helmet (empty slot)
+		itemsId = [0x00002710, 0x00099536, 0x00002710, 0x00081E91,  # Head 1   | Head 2     |    ?                               |  Armor
+				   0x0007A959, 0x0007D071, 0x00002710, 0x000933E0,  # Gloves   | Boots      |    ? (makes the head disappear xD) |  Bracelet
+				   0x00027D20, 0x00036511, 0x00098208, 0x00002710,  # Bow      | Shield     |   Costume                          |   ?
+				   0x0007F782, 0x00099562, 0x0007F3A1, 0x00081AAC, 	# ?        |  ?         |   Pants                            |   ?
+				   0x00002710, 0x00092C1A, 0x00092C1B, 0x0008DE0B];	# ?        | Ring left  |   Right right                      |  Necklace
+		hairId = 3;
+		spriteID = 0; # max 18
+
+		# Packet
+		reply  = struct.pack("<H", PacketType.BC_COMMANDER_CREATE);
+		reply += struct.pack("<I", 0xFFFFFFFF); # UNKNOWN
+		reply += charName; # Character Name
+		reply += "\x00" * (65 - len(charName));
+		reply += rightClick; # Right click description, not sure what it's for
+		reply += "\x00" * (65 - len(rightClick));
+		reply += struct.pack("<B", 0) * 14; # UNKNOWN
+		reply += struct.pack("<H", classId) # class id
+		reply += struct.pack("<H", 0) # UNKNOWN
+		reply += struct.pack("<H", jobId) # job id
+		reply += struct.pack("<B", gender) # Gender
+		reply += struct.pack("<B", 1) # UNKNOWN
+		reply += struct.pack("<I", characterLevel); # Character level
+
+		for itemId in itemsId: # Inventory : 20 items
+			reply += struct.pack("<I", itemId); # items
+
+		reply += struct.pack("<B", hairId); # Hairstyle
+		reply += struct.pack("<B",0x00) * 3; # UNKNOWN
+		
+		reply += struct.pack("<I",0x000000FF); # PCID - Still need to understand how it works
+
+		reply += struct.pack("<I", self.positionCharacterList) # Position in the character list
+		reply += struct.pack("<B", self.nbCharacterBarrack) # Character position in the character list
+		reply += struct.pack("<B", 0) # UNKNOWN
+		reply += struct.pack("<H", mapId) # mapID
+		reply += struct.pack("<I", 0) * 3; # UNKNOWN
+		reply += struct.pack("<I", spriteID) * 1; # Apparence du sprite du corps
+		reply += struct.pack("<I", 0) * 1; # UNKNOWN
+		reply += struct.pack("<I", 0) * 2; # UNKNOWN
+		reply += struct.pack("<I", 0) * 8; # UNKNOWN
+		
+		self.sock.send (reply)
+		print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
+
+			
+	def netDecrypt (self, data):
+		# TODO
+		packetSize = struct.unpack("<H", data[:2])[0];
+		print "PacketSize received : %d" % packetSize;
+		packet = data[2:];
+		return packet;
+
+		
+	def extractPacketType (self, data):
+		return struct.unpack("<H", data[:2])[0];
+
+		
+	def start (self):
 		while True:
-			print "BC_COMMANDER_CREATE Handler";
-			data = sock.recv (self.PACKETSIZE_MAX) # CB_BARRACKNAME_CHANGE
-			print 'Expected CB_COMMANDER_CREATE. Received : ' + binascii.hexlify(data)
-
-			# Size: 318
-			# Configs
-			nbCharacterBarrack += 1;
-			positionCharacterList += 1;
-			charName = "Rioru";
-			rightClick = "I'm displayed by a right click on the character";
-			classId = 10003;
-			jobId = 3;
-			gender = 1;
-			mapId = 0xD34;
-			characterLevel = 1337;
-			# 0x2710 = empty item (? or maybe it's just bugged)
-			itemsId = [0x9d89d, 0x9d89e, 0x9d89f, 0x9d8a0, 0x9d8a1, 0x2710, 0x2710, 0x2710, 0x9d8a5, 0x9d8a6, 0x9d8a7, 0x9d8a8, 0x9d8a9, 0x9d8aa, 0x9d8ab, 0x9d8ac, 0x9d8ad, 0x9d8ae, 0x9d8af, 0x9d8b0];
-			hairId = 3;
-			spriteID = 0; # max 18
-
-			# Packet
-			reply  = struct.pack("<H", PacketType.BC_COMMANDER_CREATE);
-			reply += struct.pack("<I", 0xFFFFFFFF); # UNKNOWN
-			reply += charName; # Character Name
-			reply += "\x00" * (65 - len(charName));
-			reply += rightClick; # Right click description, not sure what it's for
-			reply += "\x00" * (65 - len(rightClick));
-			reply += struct.pack("<B", 0xFF) * 14; # UNKNOWN
-			reply += struct.pack("<H", classId) # class id
-			reply += struct.pack("<H", 0xFF) # UNKNOWN
-			reply += struct.pack("<H", jobId) # job id
-			reply += struct.pack("<B", gender) # Gender
-			reply += struct.pack("<B", 1) # UNKNOWN
-			reply += struct.pack("<I", characterLevel); # Character level
-			for itemId in itemsId: # Inventory : 20 items
-				reply += struct.pack("<I", itemId); # items
-			reply += struct.pack("<B", hairId); # Hairstyle
-			reply += struct.pack("<B", 0xFF) * 3; # UNKNOWN
-			reply += struct.pack("<I", 0xFF); # UNKNOWN
-			reply += struct.pack("<I", positionCharacterList) # Position in the character list
-			reply += struct.pack("<B", nbCharacterBarrack) # Character position in the character list
-			reply += struct.pack("<B", 0xFF) # UNKNOWN
-			reply += struct.pack("<H", mapId) # mapID
-			reply += struct.pack("<I", 0xFF) * 3; # UNKNOWN
-			reply += struct.pack("<I", spriteID) * 1; # Apparence du sprite du corps
-			reply += struct.pack("<I", 0xFF) * 1; # UNKNOWN
-			reply += struct.pack("<I", 0xFF) * 2; # UNKNOWN
-			reply += struct.pack("<I", 0xFF) * 8; # UNKNOWN
-			sock.send (reply)
-			print "Sent : " + binascii.hexlify (reply) + " (" + str(len(reply)) + ")";
-
-	def start (self, sock):
-		self.loginOKHandler (sock);
-		self.startBarrackHandler (sock);
-		self.serverEntryHandler (sock);
-		self.commanderListHandler (sock);
-		self.currentBarrackHandler (sock);
-		self.barrackNameChangeHandler (sock);
-		self.commanderCreateHandler (sock);
+			data = self.sock.recv (self.PACKETSIZE_MAX)
+			packet = self.netDecrypt (data);
+			packetType = self.extractPacketType (packet);
+			
+			# Packet handler
+			if (packetType == PacketType.CB_LOGIN_BY_PASSPORT):
+				self.loginByPasswordHandler (packet);
+				
+			elif (packetType == PacketType.CB_START_BARRACK):
+				self.startBarrackHandler (packet);
+				
+			elif (packetType == PacketType.CB_CURRENT_BARRACK):
+				self.currentBarrackHandler (packet);
+				
+			elif (packetType == PacketType.CB_BARRACKNAME_CHANGE):
+				self.barrackNameChangeHandler (packet);
+				
+			elif (packetType == PacketType.CB_COMMANDER_CREATE):
+				self.commanderCreateHandler (packet);
+				
+			else:
+				print "[WARNING] Unhandled packet type = 0x%x" % packetType;
+			
 
 	def __init__ (self, sock):
 		self.sock = sock;
-		self.start (sock);
+		self.start ();
