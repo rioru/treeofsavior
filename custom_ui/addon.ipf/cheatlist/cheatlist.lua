5408a674a013a6fa05e778a4abd437784527845a
Spl3en <spl3en.contact@gmail.com>
2015-02-18 23:49:15 +0100

    Add an array storing the messages when the console isn't instancied.

--- custom_ui/addon.ipf/cheatlist/cheatlist.lua
+++ custom_ui/addon.ipf/cheatlist/cheatlist.lua
@@ -1,22 +1,35 @@
 
 cheatFrame = nil;
 dbgMsgCount = 0;
+notDisplayed = {}
 
 function CHEATLIST_ON_INIT(addon, frame)
 	cheatFrame = frame;
 end
 
+function addMsgConsole (grid, text)
+	ATTACH_TEXT_TO_OBJECT(grid, 'DBGMSG_' .. dbgMsgCount, text, 1, (dbgMsgCount * 25), 1000, 30, ui.LEFT, ui.TOP, 1, "left", "top");
+	dbgMsgCount = dbgMsgCount + 1;
+end
+
 function EMUDBG (textDbg)
 	if cheatFrame == nil then
+		table.insert(notDisplayed, textDbg);
 		return;
 	end
+
 	local grid = GET_CHILD(cheatFrame, 'r1emu_grid', 'ui::CGrid');
-	ATTACH_TEXT_TO_OBJECT(grid, 'DBGMSG_' .. dbgMsgCount, textDbg, 1, (dbgMsgCount * 35), 1000, 35, ui.LEFT, ui.TOP, 1, "left", "top");
-	dbgMsgCount = dbgMsgCount + 1;
+	
+	-- Some messages are still waiting to be displayed
+	for k,msg in pairs (notDisplayed) do
+		addMsgConsole(grid, msg);
+	end
+	notDisplayed =  {};
+	
+	addMsgConsole (grid, textDbg);
 end
 
 function CHEATLIST_UI_CLOSE(frame, obj, argStr, argNum)
 	ui.CloseFrame('cheatlist');
-	cheatFrame = nil;
 end
 
