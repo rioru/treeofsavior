52dea715db00a98d83bb2451998deb0a4c3e589f
Spl3en <spl3en.contact@gmail.com>
2015-02-18 22:59:52 +0100

    First working console. Call EMUDBG('Debug message') to add a message to the console.

--- custom_ui/addon.ipf/cheatlist/cheatlist.lua
+++ custom_ui/addon.ipf/cheatlist/cheatlist.lua
@@ -1,16 +1,22 @@
 
 cheatFrame = nil;
+dbgMsgCount = 0;
 
 function CHEATLIST_ON_INIT(addon, frame)
 	cheatFrame = frame;
 end
 
-function CHEATLIST_DBG ()
-	local dbgmsg = GET_CHILD_RECURSIVELY(cheatFrame, 'dbgmsg', 'ui::CRichText');
-	dbgmsg:SetText("{@st41b}".. "DEBUG TEST")
+function EMUDBG (textDbg)
+	if cheatFrame == nil then
+		return;
+	end
+	local grid = GET_CHILD(cheatFrame, 'r1emu_grid', 'ui::CGrid');
+	ATTACH_TEXT_TO_OBJECT(grid, 'DBGMSG_' .. dbgMsgCount, textDbg, 1, (dbgMsgCount * 35), 1000, 35, ui.LEFT, ui.TOP, 1, "left", "top");
+	dbgMsgCount = dbgMsgCount + 1;
 end
 
 function CHEATLIST_UI_CLOSE(frame, obj, argStr, argNum)
 	ui.CloseFrame('cheatlist');
+	cheatFrame = nil;
 end
 
