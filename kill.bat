598a8aa2607745ff76f788fb3ce0d14033b2999d
Spl3en <spl3en.contact@gmail.com>
2015-04-30 19:07:00 +0200

    Add scripts for launching / stopping the servers quickly on Windows.

---
+++ kill.bat
@@ -0,0 +1,3 @@
+taskkill /f /im "GlobalServer.exe"
+taskkill /f /im "BarrackServer.exe"
+taskkill /f /im "ZoneServer.exe"
---
+++ launch.bat
@@ -0,0 +1,3 @@
+cd server\R1EMU\bin\
+start GlobalServer.exe ../cfg/server.cfg
+start BarrackServer.exe ../cfg/server.cfg
