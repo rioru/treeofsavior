640016056d029b85837d13fd66ea0aca5cd50cfb
Spl3en <spl3en.contact@gmail.com>
2015-07-30 22:01:58 +0200

    Improve PacketLogger and packetTypeFinder.

--- clienthooks/HooksOfSavior/FunctionOffset.h
+++ clienthooks/HooksOfSavior/FunctionOffset.h
@@ -7,7 +7,7 @@
 #define OFFSET_LuaGetObject   (0x103B6D0 - 0x400000)
 #define OFFSET_logDebug_2     (0x0CA8E80 - 0x400000)
 #define OFFSET_GetPacket      (0x0645DC0 - 0x400000)
-#define OFFSET_NetEncrypt     (0x0CBE530 - 0x400000)
-#define OFFSET_CNetUsr__Recv  (0x0645350 - 0x400000)
+#define OFFSET_NetEncrypt     (0x0CE1920 - 0x400000) // CBT3 KR ; 30/07
+#define OFFSET_CNetUsr__Recv  (0x0645C00 - 0x400000) // CBT3 KR ; 30/07
 
 #define OFFSET_lua_gettop     (0x0CD93C0 - 0x400000)
--- re/ida/2-packetTypeFinder.py
+++ re/ida/2-packetTypeFinder.py
@@ -13,6 +13,9 @@ gePacketTable__PACKET_TABLE__AddPacketSize = 0x9902E0;
 
 f = open ('PacketType.h', 'w');
 f.write ("typedef enum PacketType {\n\n");
+packetsId = [];
+packetsName = [];
+packetsSize = [];
 
 # Rename all functions declared with gePacketTable__PACKET_TABLE__AddPacketSize
 occ = RfirstB (gePacketTable__PACKET_TABLE__AddPacketSize);
@@ -23,12 +26,26 @@ while occ != BADADDR:
     packetId = GetOperandValue (packetIdInsn, 0);
     packetSize = GetOperandValue (packetSizeInsn, 0);
     packetName = GetString (Dword (occ + 6));
+
+    packetsId.append (packetId);
+    packetsSize.append (packetSize);
+    packetsName.append (packetName);
+    
     packetLine = "\t%s = %d, // Size = %d" % (packetName, packetId, packetSize);
     f.write (packetLine + "\n");
     occ = RnextB (gePacketTable__PACKET_TABLE__AddPacketSize, occ);
 
 f.write ('\n\tPACKET_TYPE_COUNT\n\n');
-f.write ('}\tPacketType;');
+f.write ('}\tPacketType;\n\n');
+
+# Write string association
+f.write ("const PacketTypeInfo packetTypeInfo = {\n");
+f.write ("    #define REGISTER_PACKET_TYPE_ENTRY(packetType, packetValue, packetSize) \\\n");
+f.write ("        .packets[packetType] = {.value = packetValue, .size = packetSize, .string = STRINGIFY (packetType)}\n");
+for name,id,size in zip (packetsName, packetsId, packetsSize):
+    f.write ("    REGISTER_PACKET_TYPE_ENTRY (%s, %d, %d),\n" % (name, id, size));
+
+f.write ("    #undef REGISTER_PACKET_TYPE_ENTRY\n};");
 f.close ();
 
 print "The structure has been written to : %s" % (os.getcwd() + "\\PacketType.h");
