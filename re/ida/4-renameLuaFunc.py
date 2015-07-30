73cce5c3b7e4a204a2a01da6ee1c4c17d16c9b8f
Spl3en <spl3en.contact@gmail.com>
2015-07-30 21:13:50 +0200

    Update renameLuaFunc so it checks for Lua definition before.

--- re/ida/4-renameLuaFunc.py
+++ re/ida/4-renameLuaFunc.py
@@ -7,21 +7,73 @@ Automic rename of Lua related functions
 import idaapi
 import idc
 
-LuaExtern__declGlobalFunction = 0x0CD9800;
+if GetStrucIdByName ("lua_State") == BADADDR:
+    print "ERROR : Add definition of lua_State first.";
+    print '''
+typedef union {
+  void *gc;
+  void *p;
+  int n;
+  int b;
+} LuaValue;
 
-def MakeNameForce (address, name):
-    x = 2;
-    newName = name;
-    while (MakeNameEx (address, newName, SN_NOWARN) == 0):
-        newName = "%s_%d" % (name, x);
-        x = x + 1;
-    return newName;
+struct lua_TValue
+{
+  LuaValue value;
+  int tt;
+};
+struct lua_State
+{
+  void *next;
+  unsigned __int8 tt;
+  unsigned __int8 marked;
+  unsigned __int8 status;
+  StkId top;
+  StkId base;
+  void *l_G;
+  void *ci;
+  void *savedpc;
+  StkId stack_last;
+  StkId stack;
+  void *end_ci;
+  void *base_ci;
+  int stacksize;
+  int size_ci;
+  unsigned __int16 nCcalls;
+  unsigned __int16 baseCcalls;
+  unsigned __int8 hookmask;
+  unsigned __int8 allowhook;
+  int basehookcount;
+  int hookcount;
+  void *hook;
+  LuaTValue l_gt;
+  LuaTValue env;
+  void *openupval;
+  void *gclist;
+  void *errorJmp;
+};
+typedef LuaTValue *StkId;
+typedef struct lua_TValue LuaTValue;
+'''.strip();
 
-# Rename all functions declared with LuaExtern__declGlobalFunction
-occ = RfirstB (LuaExtern__declGlobalFunction);
-while occ != BADADDR:
-    routineAddress = Dword (occ - 5 - 4);
-    routineName = GetString (Dword (occ - 4));
-    occ = RnextB (LuaExtern__declGlobalFunction, occ);
-    name = MakeNameForce (routineAddress, routineName);
-    SetType (routineAddress, "int __cdecl %s (lua_State * luaState)" % name);
+else:
+    # Tip : GetSessionObject is already defined with ToSrenameDebugFunctions.py for being discovered
+    # Just look for XRef of GetSessionObject and you'll find LuaExtern__declGlobalFunction
+    LuaExtern__declGlobalFunction = 0xCFC290;
+
+    def MakeNameForce (address, name):
+        x = 2;
+        newName = name;
+        while (MakeNameEx (address, newName, SN_NOWARN) == 0):
+            newName = "%s_%d" % (name, x);
+            x = x + 1;
+        return newName;
+
+    # Rename all functions declared with LuaExtern__declGlobalFunction
+    occ = RfirstB (LuaExtern__declGlobalFunction);
+    while occ != BADADDR:
+        routineAddress = Dword (occ - 5 - 4);
+        routineName = GetString (Dword (occ - 4));
+        occ = RnextB (LuaExtern__declGlobalFunction, occ);
+        name = MakeNameForce (routineAddress, routineName);
+        SetType (routineAddress, "int __cdecl %s (lua_State * luaState)" % name);
