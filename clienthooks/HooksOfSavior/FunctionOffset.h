9b0180724789726f9363dc5a8f0ec07b6de72117
Spl3en <spl3en.contact@gmail.com>
2015-08-02 03:10:47 +0200

    Update Functions Offsets for ICBT3.

--- clienthooks/HooksOfSavior/FunctionOffset.h
+++ clienthooks/HooksOfSavior/FunctionOffset.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#define OFFSET_SetViewTM      (0x0AB4100 - 0x400000)
-#define OFFSET_SendPacket     (0x06459C0 - 0x400000)
-#define OFFSET_convertIESToIR (0x08DDF40 - 0x400000)
-#define OFFSET_logDebug_1     (0x0CB0C40 - 0x400000)
-#define OFFSET_LuaGetObject   (0x103B6D0 - 0x400000)
-#define OFFSET_logDebug_2     (0x0CA8E80 - 0x400000)
-#define OFFSET_GetPacket      (0x0645DC0 - 0x400000)
-#define OFFSET_NetEncrypt     (0x0CE1920 - 0x400000) // CBT3 KR ; 30/07
-#define OFFSET_CNetUsr__Recv  (0x0645C00 - 0x400000) // CBT3 KR ; 30/07
+// Camera
+#define OFFSET_SetViewTM      (0x0AD71B0 - 0x400000) // ICBT3
 
+// Items list
+#define OFFSET_convertIESToIR (0x08DDF40 - 0x400000)
+#define OFFSET_LuaGetObject   (0x103B6D0 - 0x400000)
+
+// Debug functions
+#define OFFSET_logDebug_1     (0x0CB0C40 - 0x400000)
+#define OFFSET_logDebug_2     (0x0CA8E80 - 0x400000)
+
+// Packets
+#define OFFSET_SendPacket     (0x064A3F0 - 0x400000) // ICBT3
+#define OFFSET_GetPacket      (0x064A7F0 - 0x400000) // ICBT3
+#define OFFSET_NetEncrypt     (0x0CE5230 - 0x400000) // ICBT3
+#define OFFSET_CNetUsr__Recv  (0x0649D60 - 0x400000) // ICBT3
+
+// Lua
 #define OFFSET_lua_gettop     (0x0CD93C0 - 0x400000)
